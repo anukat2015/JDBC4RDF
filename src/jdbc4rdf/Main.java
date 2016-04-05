2d5f039e4560dc3b858b20ecec28ac5f9384251e
timo <timoei@users.noreply.github.com>
2016-04-05 13:08:32 +0200

    Added support of execution.

--- src/jdbc4rdf/Main.java
+++ src/jdbc4rdf/Main.java
@@ -1,11 +1,16 @@
 package jdbc4rdf;
 
 import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 
 import jdbc4rdf.core.config.Config;
 import jdbc4rdf.core.config.DBDRIVER;
+import jdbc4rdf.core.config.ExecutorConfig;
 import jdbc4rdf.core.config.LoaderConfig;
+import jdbc4rdf.executor.ParseFile;
+import jdbc4rdf.executor.Query;
 import jdbc4rdf.loader.SQLDataLoader;
 import jdbc4rdf.loader.impl.HiveDataLoader;
 import jdbc4rdf.loader.impl.MySQLDataLoader;
@@ -24,11 +29,12 @@ public class Main {
 		logger.info(DBDRIVER.getDriverList() + "\n");
 		logger.info("SCALEUB should be a value between 0.0 and 1.0");
 		logger.info("> Executor Syntax:");
-		logger.info("exec TODO...TODO");
-	}
+		logger.info("exec DRIVER FILE HOST [DB] USER PW");
+		logger.info("Supported drivers: ");
+		logger.info(DBDRIVER.getDriverList() + "\n");
+	}	
 	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// Parse all the arguments
 		
 		Config conf;
@@ -81,10 +87,49 @@ public class Main {
 				sql.runSql();
 				
 			} else if (args[0].equals("exec")) {
-				// TODO: execConfig
-				// conf = new ExecuterConfig(...);
-				// QueryExecuter qe = new  (Hive?)QueryExecuter(conf) ?? ....
-				logger.log(Level.INFO, "Not yet implemented!");
+				
+				String file = "";
+				String host = "";
+				String db = "";
+				String user = "";
+				String pw = "";
+				
+				if(args.length == 7){
+					driver = DBDRIVER.detectDriver(args[1]);
+					file = args[2];
+					host = args[3];
+					db = args[4];
+					user = args[5];
+					pw = args[6];
+				}
+				else if (args.length == 6) {
+					driver = DBDRIVER.detectDriver(args[1]);
+					file = args[2];
+					host = args[3];
+					db = "";
+					user = args[4];
+					pw = args[5];
+				} else {
+					logger.log(Level.SEVERE, "Not enough arguments given");
+					showHelp();
+					return;
+				}
+				// Create config
+				conf = new ExecutorConfig(driver, file, user, pw, host, db);
+				
+				/*
+				 * // Detect driver
+				SQLDataLoader sql = null;
+				if (driver.equals(DBDRIVER.HIVE)) {
+					sql = new HiveDataLoader(conf);
+				} else if (driver.equals(DBDRIVER.MYSQL)) {
+					sql = new MySQLDataLoader(conf);
+				}
+				// run sql
+				sql.runSql();
+				 */
+				List<Query> queries = ParseFile.getQueries(file);
+				
 			}
 		} else {
 			logger.log(Level.SEVERE, "Not enough arguments given");

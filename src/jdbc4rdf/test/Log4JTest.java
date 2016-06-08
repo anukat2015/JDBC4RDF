8e796af61982b175e9f4c53b7f17da29cefd38f7
Max <mhofmann91@gmail.com>
2016-06-08 08:49:31 +0200

    Fixed gitignore to exclude the test sub-package

---
+++ src/jdbc4rdf/test/Log4JTest.java
@@ -0,0 +1,36 @@
+package jdbc4rdf.test;
+
+import org.apache.log4j.Logger;
+
+public class Log4JTest {
+
+	
+	final static Logger logger = Logger.getLogger(Log4JTest.class);
+	
+	
+	public void test() {
+
+		System.out.println("* Log4j Test Start *");
+		
+		System.out.println();
+		
+		System.out.println("> Looking for config in:");
+		System.out.println(Thread.currentThread().getContextClassLoader().getResource("") + "\n");
+		
+		logger.debug("Debug message");
+		logger.info("Info message");
+		logger.warn("Warning message");
+		logger.error("Error message");
+		logger.fatal("Fatal message");
+		
+		System.out.println();
+		
+		System.out.println("* Log4j Test End *");
+	}
+	
+	public static void main(String[] args) {
+		Log4JTest l4j = new Log4JTest();
+		l4j.test();
+	}
+
+}
